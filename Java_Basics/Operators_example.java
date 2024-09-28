a1f620daef07af3b4d714717791a461a624c7f34
Govind409 <125127351+Govind409@users.noreply.github.com>
2024-09-28 21:46:45 +0530

    For verifying git

--- Java_Basics/Operators_example.java
+++ Java_Basics/Operators_example.java
@@ -3,9 +3,9 @@ package Java_Basics;
 public class Operators_example {
 
 	public static void main(String[] args) {
-		int num1 = 4,num2=5;
-		int largest=(num1>num2)? num1 : num2;
-        System.out.println(largest);
+		int num1 = 4, num2 = 5;
+		int largest = (num1 > num2) ? num1 : num2;
+		System.out.println(largest);
 
 	}
 
